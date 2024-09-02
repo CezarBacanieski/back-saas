02bd53f176439537a02db29d63717b6b69975d7d
CezarDev <bacanieskidev@gmail.com>
2024-09-02 12:34:53 -0300

    feat: Implementa funções de exclusão e atualização de linhas na API
    - Adiciona o método `excluirLinha` para deletar uma linha da tabela com base no ID fornecido.
    - Adiciona o método `atualizarLinha` para atualizar os dados de uma linha específica na tabela, utilizando o ID como referência.
    - Melhora o tratamento de erros para retornar mensagens claras em caso de falha na exclusão ou atualização.

--- src/controllers/tabelaController.js
+++ src/controllers/tabelaController.js
@@ -1,85 +1,98 @@
 import { tabelaResultadosModel } from '../models/index.js';
 
 class TabelaController {
-	static buscarTodasTabelaResultado = async (req, res) => {
-		try {
-			const todasAsTabelasResultado = await tabelaResultadosModel.find({});
-			res.status(200).json(todasAsTabelasResultado);
-		} catch (erro) {
-			res
-				.status(404)
-				.json({ message: 'falha ao buscar todas as tabelas' + erro });
-		}
-	};
+  static buscarTodasTabelaResultado = async (req, res) => {
+    try {
+      const todasAsTabelasResultado = await tabelaResultadosModel.find({});
+      res.status(200).json(todasAsTabelasResultado);
+    } catch (erro) {
+      res
+        .status(404)
+        .json({ message: 'falha ao buscar todas as tabelas' + erro });
+    }
+  };
 
-	static buscarTabelaPorNome = async (req, res) => {
-		try {
-			const nome = req.params.nome;
-			const tabelaResultadoBuscaPorNome = await tabelaResultadosModel.find({
-				nome: nome,
-			});
-			res.status(200).json(tabelaResultadoBuscaPorNome);
-		} catch (erro) {
-			res
-				.status(404)
-				.json({ message: 'falha ao buscar tabela por nome' + erro });
-		}
-	};
+  static buscarTabelaPorNome = async (req, res) => {
+    try {
+      const { nome } = req.params.nome;
+      const tabelaResultadoBuscaPorNome = await tabelaResultadosModel.find({
+        nome: nome,
+      });
+      res.status(200).json(tabelaResultadoBuscaPorNome);
+    } catch (erro) {
+      res
+        .status(404)
+        .json({ message: 'falha ao buscar tabela por nome' + erro });
+    }
+  };
 
-	static excluirTabelaPorNome = async (req, res) => {
-		try {
-			const { nome } = req.params;
-			const tabelaParaSerExcluida = await tabelaResultadosModel.deleteMany({
-				nome: nome,
-			});
-			if (tabelaParaSerExcluida.deletedCount > 0) {
-				res.status(200).json({
-					message: `Tabelas com o nome '${nome}' excluídas com sucesso.`,
-				});
-			} else {
-				res.status(404).send('Não foi encontrada tabela com esse nome');
-			}
+  static excluirLinha = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await tabelaResultadosModel.findByIdAndDelete(id);
 
-			// outro metodo que pode ser usado
-			// const tabelaParaSerExcluida = await tabelaResultadosModel.find({
-			// 	nome: nome,
-			// });
+      res.status(200).send('Linha excluida com sucesso');
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao excluir linha', error });
+    }
+  };
 
-			// if (tabelaParaSerExcluida.length > 0) {
-			// 	const deletions = tabelaParaSerExcluida.map(async (tabela) => {
-			// 		return await tabelaResultadosModel.deleteOne({ _id: tabela._id });
-			// 	});
+  static atualizarLinha = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const dadosAtualizados = req.body;
 
-			// 	await Promise.all(deletions);
-			// 	res.status(200).json({
-			// 		message:
-			// 			`Tabelas com o nome ${nome} excluida com sucesso: ` +
-			// 			tabelaParaSerExcluida,
-			// 	});
-		} catch (erro) {
-			res
-				.status(500)
-				.json({ message: 'Erro ao excluir tabela por nome' + erro });
-		}
-	};
+      const response = await tabelaResultadosModel.findByIdAndUpdate(
+        id,
+        { $set: dadosAtualizados },
+        { new: true }
+      );
 
-	static enviarResultados = async (req, res) => {
-		try {
-			const nome = req.body.nome;
-			const itensTabela = req.body.itens;
+      res
+        .status(200)
+        .json({ message: 'Dados da linha atualizados com sucesso', response });
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao atualizar linha' + error });
+    }
+  };
 
-			const tabelaResultadoComNome = itensTabela.map((item) => ({
-				...item,
-				nome,
-			}));
-			await tabelaResultadosModel.insertMany(tabelaResultadoComNome);
-			res.status(200).json({ message: 'Tabela salva com sucesso' });
-		} catch (erro) {
-			res.status(500).json({
-				message: `Erro ao enviar resultados da tabela ${erro.message} `,
-			});
-		}
-	};
+  static excluirTabelaPorNome = async (req, res) => {
+    try {
+      const { nome } = req.params;
+      const tabelaParaSerExcluida = await tabelaResultadosModel.deleteMany({
+        nome: nome,
+      });
+      if (tabelaParaSerExcluida.deletedCount > 0) {
+        res.status(200).json({
+          message: `Tabelas com o nome '${nome}' excluídas com sucesso.`,
+        });
+      } else {
+        res.status(404).send('Não foi encontrada tabela com esse nome');
+      }
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: 'Erro ao excluir tabela por nome' + erro });
+    }
+  };
+
+  static enviarResultados = async (req, res) => {
+    try {
+      const nome = req.body.nome;
+      const itensTabela = req.body.itens;
+
+      const tabelaResultadoComNome = itensTabela.map((item) => ({
+        ...item,
+        nome,
+      }));
+      await tabelaResultadosModel.insertMany(tabelaResultadoComNome);
+      res.status(200).json({ message: 'Tabela salva com sucesso' });
+    } catch (erro) {
+      res.status(500).json({
+        message: `Erro ao enviar resultados da tabela ${erro.message} `,
+      });
+    }
+  };
 }
 
 export default TabelaController;
--- src/routes/tabelaResultadosRoutes.js
+++ src/routes/tabelaResultadosRoutes.js
@@ -6,6 +6,11 @@ const router = express.Router();
 router.get('/todosResultados', TabelaController.buscarTodasTabelaResultado);
 router.get('/buscarTabelaPorNome/:nome', TabelaController.buscarTabelaPorNome);
 router.post('/salvar', TabelaController.enviarResultados);
-router.delete('/excluirTabelaPorNome/:nome', TabelaController.excluirTabelaPorNome);
+router.put('/atualizarLinha/:id', TabelaController.atualizarLinha);
+router.delete(
+  '/excluirTabelaPorNome/:nome',
+  TabelaController.excluirTabelaPorNome
+);
+router.delete('/excluirLinha/:id', TabelaController.excluirLinha);
 
 export default router;
